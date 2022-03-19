276a07a37aba4129bbed87d1f3224015f8c7b3eb
cotsom <gerfy.17@mail.ru>
2022-03-19 23:11:40 +0300

    get response

--- Scanner/src/Fuzz.java
+++ Scanner/src/Fuzz.java
@@ -1,9 +1,10 @@
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.params.ClientPNames;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.LaxRedirectStrategy;
 
 import java.io.IOException;
 
@@ -11,10 +12,14 @@ import java.io.IOException;
 public class Fuzz {
     void dirScan(String url) throws IOException {
         String Url = "http://"+url+"/robots.txt";
-        HttpClient httpClient = HttpClients.createDefault();
+        //HttpClient httpClient = HttpClients.createDefault();
+        HttpClient httpClient = HttpClientBuilder.create().setRedirectStrategy(new LaxRedirectStrategy()).build();
         HttpGet httpGet = new HttpGet(Url);
         HttpResponse httpResponse = httpClient.execute(httpGet);
-        String body = EntityUtils.toString(httpResponse.getEntity());
-        System.out.println(body);
+        String response = String.valueOf(httpResponse.getStatusLine());
+        System.out.println(response);
+        if (response.equals("HTTP/1.1 200 OK")){
+            System.out.println("");
+        }
     }
 }
